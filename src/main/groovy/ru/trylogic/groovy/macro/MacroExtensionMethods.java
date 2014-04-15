7a8717e9fcdf195914c7ded4fbbe33587b82deba
Sergey Egorov <bsideup@gmail.com>
2014-04-15 13:01:20 +0400

    $v extension method is replaced with ValuePlaceholder delegate

--- src/main/groovy/ru/trylogic/groovy/macro/MacroExtensionMethods.java
+++ src/main/groovy/ru/trylogic/groovy/macro/MacroExtensionMethods.java
@@ -1,17 +1,21 @@
 package ru.trylogic.groovy.macro;
 
 import groovy.lang.Closure;
+import groovy.lang.DelegatesTo;
 
 public class MacroExtensionMethods {
-    public static <T> T macro(Object self, Closure cl) {
+    
+    public static class ValuePlaceholder {
+        public static Object $v(Closure cl) {
+            return null; // replaced with AST transformations
+        }
+    }
+    
+    public static <T> T macro(Object self, @DelegatesTo(ValuePlaceholder.class) Closure cl) {
         return null;
     }
 
-    public static <T> T macro(Object self, boolean asIs, Closure cl) {
-        return null;
-    }
-
-    public static Object $v(Object self, Closure cl) {
+    public static <T> T macro(Object self, boolean asIs, @DelegatesTo(ValuePlaceholder.class) Closure cl) {
         return null;
     }
 }
